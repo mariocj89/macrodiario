27967b2353d1c3fbb46ae3b5cdb729bf4d3db7f3
Mario Corchero <mariocj89@gmail.com>
2023-07-19 11:06:04 +0200

    Add Radar chart to month view

--- src/screens/CalendarScreen.js
+++ src/screens/CalendarScreen.js
@@ -1,9 +1,10 @@
 import { useEffect, useState, useContext } from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View, Text, ScrollView } from "react-native";
 import { Calendar, LocaleConfig } from "react-native-calendars";
 import Storage from "../storage";
 import DateStr from "../dateStr";
 import StateContext from "../context/stateProvider";
+import PolarChart from "../components/PolarChart";
 
 LocaleConfig.locales["es"] = {
   monthNames: [
@@ -84,37 +85,130 @@ const makeMarkedDay = (dayData) => {
   };
 };
 
+const translation = {
+  vegetables: "Verduras",
+  proteins: "Proteinas",
+  carbs: "Carbohidratos",
+  fats: "Grasas",
+  fruits: "Fruta",
+};
+
+const loadAggregatedData = async (year, month, action) => {
+  const monthData = await Storage.getMonthData(year, month);
+  var maxTakes = {};
+  var takes = {};
+  for (const [date, dayData] of Object.entries(monthData)) {
+    for (const [macroKey, macro] of Object.entries(translation)) {
+      const value = dayData.maxTakes[macroKey];
+      if (value === 0) {
+        continue;
+      }
+      maxTakes[macro] = maxTakes[macro] ?? 0;
+      maxTakes[macro] += value;
+    }
+    for (const [macroKey, macro] of Object.entries(translation)) {
+      const value = dayData.takes[macroKey];
+      if (value === 0) {
+        continue;
+      }
+      takes[macro] = takes[macro] ?? 0;
+      takes[macro] += value;
+    }
+  }
+  Object.keys(maxTakes).forEach((key) => {
+    takes[key] = takes[key] ?? 0;
+  });
+  console.log("Aggregated maxTakes:", maxTakes);
+  console.log("Aggregated takes:", takes);
+  action([maxTakes, takes]);
+};
+
 const CalendarScreen = ({ navigation }) => {
   const [state, manager] = useContext(StateContext);
-  const [markedDays, setMarkedDays] = useState({});
+  const [calendarDayData, setCalendarDayData] = useState({});
+  const [summaryData, setSummaryData] = useState([{}, {}]);
   useEffect(() => {
     loadMonthData(
       new Date(state.date).getFullYear(),
       new Date(state.date).getMonth(),
-      setMarkedDays,
+      setCalendarDayData
+    );
+    loadAggregatedData(
+      new Date(state.date).getFullYear(),
+      new Date(state.date).getMonth(),
+      setSummaryData
     );
   }, []);
+
   return (
     <>
-      <Calendar
-        markingType="multi-dot"
-        markedDates={markedDays}
-        initialDate={state.date}
-        hideExtraDays={true}
-        theme={{ selectedDayBackgroundColor: "white" }}
-        onDayPress={(date) => {
-          const newDate = DateStr.dateToStr(new Date(date.dateString));
-          manager.setDay(newDate);
-          navigation.navigate("DayInput");
-        }}
-        onMonthChange={(date) => {
-          setMarkedDays({});
-          loadMonthData(date.year, date.month - 1, setMarkedDays);
-        }}
-        disabledByDefault
-        disableAllTouchEventsForDisabledDays
-        enableSwipeMonths
-      />
+      <ScrollView>
+        <Calendar
+          markingType="multi-dot"
+          markedDates={calendarDayData}
+          initialDate={state.date}
+          hideExtraDays={true}
+          theme={{ selectedDayBackgroundColor: "white" }}
+          onDayPress={(date) => {
+            const newDate = DateStr.dateToStr(new Date(date.dateString));
+            manager.setDay(newDate);
+            navigation.navigate("DayInput");
+          }}
+          onMonthChange={(date) => {
+            loadMonthData(date.year, date.month - 1, setCalendarDayData);
+            loadAggregatedData(date.year, date.month - 1, setSummaryData);
+          }}
+          disabledByDefault
+          disableAllTouchEventsForDisabledDays
+          enableSwipeMonths
+        />
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-evenly",
+            paddingTop: 15,
+          }}
+        >
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <View
+              style={{
+                width: 25,
+                height: 25,
+                borderWidth: 1,
+                marginRight: 10,
+                backgroundColor: "green",
+              }}
+            />
+            <Text>Objetivo</Text>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <View
+              style={{
+                width: 25,
+                height: 25,
+                borderWidth: 1,
+                marginRight: 10,
+                backgroundColor: "blue",
+              }}
+            />
+            <Text>Actual</Text>
+          </View>
+        </View>
+        <PolarChart inputData={summaryData} />
+      </ScrollView>
     </>
   );
 };
